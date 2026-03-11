c76fe35b973d1c1ac47e79928a90d0f11eaf5ab4
NIVETHAG <nivethagopi05@gmail.com>
2026-03-11 09:38:57 +0530

    Initial commit

--- Pocket-doctor-2-main/.idx/dev.nix
+++ Pocket-doctor-2-main/.idx/dev.nix
@@ -7,7 +7,7 @@
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
-  ];
+
   # Sets environment variables in the workspace
   env = {};
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
